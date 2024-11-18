444ab63131e8dd5dca5fc55cddbb76631a56936e
Hauwa <hazykareem@gmail.com>
2024-11-18 08:27:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def elfin_function(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using make_response()
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'Elfin'
+    
+    return f'Hello {name}! Your input has been processed.'
